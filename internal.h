07750194bc3ac4415ae1d85946f9cd5913bf4c8a
Attie Grande <attie@attie.co.uk>
2014-03-11 19:26:19 +0000

    moved to LGPLv3 and added README

--- internal.h
+++ internal.h
@@ -1,6 +1,25 @@
 #ifndef INTERNAL_H
 #define INTERNAL_H
 
+/*
+	magicsock - a magical socket library
+
+	Copyright (C) 2014 onwards  Attie Grande (attie@attie.co.uk)
+
+	magicsock is free software: you can redistribute it and/or modify it
+	under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	magicsock is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with magicsock. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #define LH      fprintf(stderr, "%s:%d %s() LOG HERE!\n", __FILE__, __LINE__, __FUNCTION__);
 #define LHP(p)  fprintf(stderr, "%s:%d %s() LOG HERE! - " #p " @ %p\n", __FILE__, __LINE__, __FUNCTION__, p);
 
